9afb3fc9117da50e7fa1bd062211f0850d436621
Nathan Springman <naspringman@gmail.com>
2021-07-19 08:34:46 -0400

    add subdivisions

--- src/components/PreviousIssues.js
+++ src/components/PreviousIssues.js
@@ -10,29 +10,49 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 const THUMBNAIL_URL = 'https://studlife.com/media/pdf/';
 
 const PreviousIssues = ({ previousIssues }) => {
-  const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = useState(false);
+  const [subExpanded, setSubExpanded] = useState(false);
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
 
+  const handleSubdivisionChange = (panel) => (event, isExpanded) => {
+    setSubExpanded(isExpanded ? panel : false);
+  };
+
+  const subdivisionAccordion = (subdivisions, year) => Object.entries(subdivisions).map((e) => {
+    const division = e[0];
+    return (
+      <Accordion expanded={subExpanded === `panel-${year}-${division}`} onChange={handleSubdivisionChange(`panel-${year}-${division}`)}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls={`${year}-${division}-content`}
+          id={`${year}-${division}-header`}
+        >
+          {division}
+        </AccordionSummary>
+        <AccordionDetails>
+          'test'
+        </AccordionDetails>
+      </Accordion>
+    );
+  });
+
   const yearsAccordion = () => {
     const elements = Object.entries(previousIssues).map((e) => {
       const year = e[0];
       return (
-        <Accordion expanded={expanded === `panel1-${year}`} onChange={handleChange(`panel1-${year}`)}>
+        <Accordion expanded={expanded === `panel-${year}`} onChange={handleChange(`panel-${year}`)}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls={`${year}-content`}
             id={`${year}-header`}
           >
-            {e[0]}
+            {year}
           </AccordionSummary>
           <AccordionDetails>
-            <Typography>
-              Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
-              maximus est, id dignissim quam.
-            </Typography>
+            {subdivisionAccordion(e[1], year)}
           </AccordionDetails>
         </Accordion>
       );
